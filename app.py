9c148306813fc7ac53e8da5760f81a854fa00611
Hidde Uittenbosch <hiddeuittenbosch@hotmail.com>
2021-06-09 12:18:33 +0200

    update app.py optimizing canvas and pencil, adding the model

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ import numpy as np
 from PIL import Image
 import sys
 import base64
-
+from tensorflow.keras.backend import expand_dims
+from tensorflow.keras.models import load_model
 np.set_printoptions(threshold=sys.maxsize)
+
 
 # Specify canvas parameters in application
 # stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
@@ -18,26 +20,24 @@ np.set_printoptions(threshold=sys.maxsize)
 # drawing_mode = st.sidebar.selectbox(
 #     "Drawing tool:", ("freedraw", "line", "rect", "circle", "transform"))
 # realtime_update = st.sidebar.checkbox("Update in realtime", True)
-
+#
 # Create a canvas component
 canvas_result = st_canvas(
     #fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-    stroke_width=3, #to play around with or adapt through testing model
+    stroke_width=8.2,
     stroke_color="rgba(0, 0, 0,1)",
     #background_color="" if bg_image else bg_color,
     #background_image=Image.open(bg_image) if bg_image else None,
     update_streamlit=True,
-    height=400,
-    width=400,
+    height=280,
+    width=280,
     drawing_mode="freedraw",
     key="canvas",
 )
 # Do something interesting with the image data and paths
-
-if canvas_result.json_data is not None:
-    st.dataframe(pd.json_normalize(canvas_result.json_data["objects"]))
+#if canvas_result.json_data is not None:
+    #st.dataframe(pd.json_normalize(canvas_result.json_data["objects"]))
     #df = pd.json_normalize(canvas_result.json_data["objects"])["path"]
-
 if canvas_result.image_data is not None:
     #st.image(canvas_result.image_data)
     img = Image.fromarray(canvas_result.image_data.astype(np.uint8))
@@ -45,9 +45,17 @@ if canvas_result.image_data is not None:
     img_28_28 = np.asarray(img_28_28)
     img_gray = 255 - img_28_28[:, :, 3]
     img_gray = np.invert(img_gray)
-    img_gray = np.reshape(img_gray, (784, 1))
-    print("##################################")
-    print(img_gray)
-    #st.image(img_gray)
-    st.write(img_gray)
-    
+    img_gray = img_gray.reshape((1, 28, 28,1))
+    #img_gray.shape
+    model = load_model('model.save')
+    y_pred = model.predict(img_gray)
+    y_pred = pd.DataFrame(y_pred)
+    maxValueIndex = y_pred.idxmax(axis=1)
+    pred = [
+        'alarmclock', 'bicycle', 'boomerang', 'bread', 'broccoli', 'banana',
+        'baseball', 'basketball', 'birthday cake', 'bracelet', 'cookie', 'hat',
+        'lollipop', 'moon', 'pizza', 'spoon', 'sun', 'umbrella', 'wheel',
+        'lantern', 'lightning', 'streetlight', 'tennis racquet', 'tractor'
+    ]
+
+    pred[maxValueIndex[0]]
