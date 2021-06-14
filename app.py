fc70e358799b69414387421387048f39870dd96e
Hidde Uittenbosch <hiddeuittenbosch@hotmail.com>
2021-06-14 10:24:21 +0200

    no canvas no output

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import streamlit as st
 from streamlit.caching import cache
 from streamlit.elements import button
 from streamlit_drawable_canvas import st_canvas
-from pyecharts import options as opts
-from pyecharts.charts import Bar
 from streamlit_echarts import st_echarts
 import numpy as np
 import json
@@ -29,7 +27,6 @@ def fivelabels(num):
         st.write(labels[i])
 
 with col1:
-
     if st.button('Press me!'):
         labels = np.load('labels.npy')
         fivelabels = random.sample(range(0, len(labels)), 5)
@@ -69,61 +66,67 @@ with col2:
 #st.dataframe(pd.json_normalize(canvas_result.json_data["objects"]))
 #df = pd.json_normalize(canvas_result.json_data["objects"])["path"]
 with col3:
+    
     if canvas_result.image_data is not None:
-        #st.image(canvas_result.image_data)
         img = Image.fromarray(canvas_result.image_data.astype(np.uint8))
         img_28_28 = img.resize((28, 28), Image.ANTIALIAS)
         img_28_28 = np.asarray(img_28_28)
         img_gray = 255 - img_28_28[:, :, 3]
         img_gray = np.invert(img_gray)
-        
-        #optimizing images 
-        img_gray[img_gray < 50] = 0
-        img_gray[(img_gray > 51) & (img_gray < 100)] = 75
-        img_gray[(img_gray > 101) & (img_gray < 149)] = 149
-        img_gray[img_gray > 150] = 255
-        
-        img_list = img_gray.tolist()
-        img_json = json.dumps(img_list)
-        # enter here the address of your flask api
-        url = 'https://aipictionaryimage-djqbxeaiha-ew.a.run.app/predict'
-        #url = 'http://127.0.0.1:8000/predict'
-        params = dict(img_frontend=img_json)
-        response = requests.get(url, params=params)
-        prediction = response.json()
-        df = pd.read_json(prediction)
-        fig, ax = plt.subplots()
-        #ax = sns.barplot(y=df.values[0], x=df.columns)
-        #st.pyplot(fig)
-        #st_echarts(type=pie, )
-        
-        options = {
-            "tooltip": {"trigger": "item"},
-            "legend": {"top": "5%", "left": "center"},
-            "series": [
-                {
-                    "name": "Probability",
-                    "type": "pie",
-                    "radius": ["40%", "70%"],
-                    "avoidLabelOverlap": False,
-                    "itemStyle": {
-                        "borderRadius": 10,
-                        "borderColor": "#fff",
-                        "borderWidth": 2,
-                    },
-                    "label": {"show": False, "position": "center"},
-                    "emphasis": {
-                        "label": {"show": True, "fontSize": "40", "fontWeight": "bold"}
-                    },
-                    "labelLine": {"show": False},
-                    "data": [
-                        {"value": df.values[0][0], "name": df.columns[0]},
-                        {"value": df.values[0][1], "name": df.columns[1]},
-                        {"value": df.values[0][2], "name": df.columns[2]},
-                        {"value": df.values[0][3], "name": df.columns[3]},
-                        {"value": df.values[0][4], "name": df.columns[4]},
-                    ],
-                }
-            ],
-        }
-        st_echarts(options=options, height="500px")
+        nocanvas = (img_gray.sum() == 0)
+    
+        if nocanvas != True:
+            #st.image(canvas_result.image_data)
+            print(img_gray)
+            print('----'*100)
+            #optimizing images 
+            img_gray[img_gray < 50] = 0
+            img_gray[(img_gray > 51) & (img_gray < 100)] = 75
+            img_gray[(img_gray > 101) & (img_gray < 149)] = 149
+            img_gray[img_gray > 150] = 255
+    
+
+            img_list = img_gray.tolist()
+            img_json = json.dumps(img_list)
+            # enter here the address of your flask api
+            url = 'https://aipictionaryimage-djqbxeaiha-ew.a.run.app/predict'
+            #url = 'http://127.0.0.1:8000/predict'
+            params = dict(img_frontend=img_json)
+            response = requests.get(url, params=params)
+            prediction = response.json()
+            df = pd.read_json(prediction)
+            fig, ax = plt.subplots()
+            #ax = sns.barplot(y=df.values[0], x=df.columns)
+            #st.pyplot(fig)
+            #st_echarts(type=pie, )
+            
+            options = {
+                "tooltip": {"trigger": "item"},
+                "legend": {"top": "5%", "left": "center"},
+                "series": [
+                    {
+                        "name": "Probability",
+                        "type": "pie",
+                        "radius": ["40%", "70%"],
+                        "avoidLabelOverlap": False,
+                        "itemStyle": {
+                            "borderRadius": 10,
+                            "borderColor": "#fff",
+                            "borderWidth": 2,
+                        },
+                        "label": {"show": False, "position": "center"},
+                        "emphasis": {
+                            "label": {"show": True, "fontSize": "40", "fontWeight": "bold"}
+                        },
+                        "labelLine": {"show": False},
+                        "data": [
+                            {"value": round(df.values[0][0] * 100), "name": df.columns[0]},
+                            {"value": round(df.values[0][1] * 100), "name": df.columns[1]},
+                            {"value": round(df.values[0][2] * 100), "name": df.columns[2]},
+                            {"value": round(df.values[0][3] * 100), "name": df.columns[3]},
+                            {"value": round(df.values[0][4] * 100), "name": df.columns[4]},
+                        ],
+                    }
+                ],
+            }
+            st_echarts(options=options, height="500px")
