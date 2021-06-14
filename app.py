2f717077ca448f507d3e7394c89d9d0e80fb358d
Andreas Mussger <andreas.mussger@gmail.com>
2021-06-14 11:14:08 +0200

    updated app.py to match the most recent docker image API

--- app.py
+++ app.py
@@ -16,25 +16,18 @@ st. set_page_config(layout="wide")
 col1, col2, col3 = st.beta_columns((2,3,3))
 
 col1.header('Wanna play a game?')
-
-
-@st.cache(suppress_st_warning=True)
-def fivelabels(num):
-    #button = st.button('Press me!')
-    labels = np.load('labels.npy')
-    fivelabels = random.sample(range(0, len(labels)), num)
-    for i in fivelabels:
-        st.write(labels[i])
-
 with col1:
+    choose_model = st.selectbox(
+    'Choose a model',
+    ('CNN_50', 'CNN_150', 'CNN_250'))
     if st.button('Press me!'):
-        labels = np.load('labels.npy')
+        labels = np.load(f'{choose_model}_labels.npy')
         fivelabels = random.sample(range(0, len(labels)), 5)
         for i in fivelabels:
             st.write(labels[i])
-                
-col2.header('Start drawing')
-col3.header('Most likely drawing')
+
+col2.header('Draw Something')
+col3.header('A.I. Prediction')
 
 # Specify canvas parameters in application
 # stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
@@ -60,13 +53,14 @@ with col2:
         drawing_mode="freedraw",
         key="canvas",
     )
-    
+
 # Do something interesting with the image data and paths
 #if canvas_result.json_data is not None:
 #st.dataframe(pd.json_normalize(canvas_result.json_data["objects"]))
 #df = pd.json_normalize(canvas_result.json_data["objects"])["path"]
 with col3:
-    
+
+
     if canvas_result.image_data is not None:
         img = Image.fromarray(canvas_result.image_data.astype(np.uint8))
         img_28_28 = img.resize((28, 28), Image.ANTIALIAS)
@@ -74,24 +68,24 @@ with col3:
         img_gray = 255 - img_28_28[:, :, 3]
         img_gray = np.invert(img_gray)
         nocanvas = (img_gray.sum() == 0)
-    
+
         if nocanvas != True:
             #st.image(canvas_result.image_data)
-            print(img_gray)
-            print('----'*100)
-            #optimizing images 
+            #print(img_gray)
+            #print('----'*100)
+            #optimizing images
             img_gray[img_gray < 50] = 0
             img_gray[(img_gray > 51) & (img_gray < 100)] = 75
             img_gray[(img_gray > 101) & (img_gray < 149)] = 149
             img_gray[img_gray > 150] = 255
-    
+
 
             img_list = img_gray.tolist()
             img_json = json.dumps(img_list)
             # enter here the address of your flask api
             url = 'https://aipictionaryimage-djqbxeaiha-ew.a.run.app/predict'
             #url = 'http://127.0.0.1:8000/predict'
-            params = dict(img_frontend=img_json)
+            params = dict(img_frontend=img_json, model_type=choose_model)
             response = requests.get(url, params=params)
             prediction = response.json()
             df = pd.read_json(prediction)
@@ -99,7 +93,7 @@ with col3:
             #ax = sns.barplot(y=df.values[0], x=df.columns)
             #st.pyplot(fig)
             #st_echarts(type=pie, )
-            
+
             options = {
                 "tooltip": {"trigger": "item"},
                 "legend": {"top": "5%", "left": "center"},
@@ -130,3 +124,5 @@ with col3:
                 ],
             }
             st_echarts(options=options, height="500px")
+        else: 
+            st.image("cartoon.jpeg")
